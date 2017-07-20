e5714f47a20351d937c2873bf9a3d2731b9bcea2
tsvi208 <thezandan@gmail.com>
2017-07-19 21:37:22 -0500

    bla bla

--- TrafficControlSystem/AbstractTrafficLightController.cs
+++ TrafficControlSystem/AbstractTrafficLightController.cs
@@ -6,7 +6,6 @@ namespace TrafficControlSystem
     {
     //    private View view;
     //    private Model model;
-    ;
     //    public View getView()
     //    {
     //        return this.view;
