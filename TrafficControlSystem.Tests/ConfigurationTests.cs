2bca77414fca81a7202c497434305063e1ef8043
tsvi208 <thezandan@gmail.com>
2017-08-01 23:23:35 -0500

    ..

--- TrafficControlSystem.Tests/ConfigurationTests.cs
+++ TrafficControlSystem.Tests/ConfigurationTests.cs
@@ -192,45 +192,37 @@ namespace TrafficControlSystem.Tests
         [TestMethod]
         public void TestCollision()
         {   
-            Assert.IsFalse(RunLightTest());   
+            Assert.IsFalse(RunLightTest(5));//run for 5 seconds
+            //Assert.IsFalse(RunLightTest(300));//run for 300 seconds = 5 minutes (if you dare...)
         }
 
-        public Boolean RunLightTest()
+
+        public Boolean RunLightTest(int duration)
         {
             var configuration = Configuration.Load("universityblvd_sunriserd.txt");
             
             var simulator = new IntersectionController(configuration.Intersections[0]);
 
-            //Run for 5 seconds
-            simulator.Run(new TimeSpan(0, 0, 5));
+            //Run for "duration" seconds
+            
+            simulator.Run(new TimeSpan(0, 0, duration));
+           
 
             //checking for concurrent green lights on "sunriserd_north_south" and "universityblvd_east_west_turnlanes" 
-            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[0].Signals[0].CurrentLight) && (configuration.Intersections[0].SignalGroups[0].Signals[0].CurrentLight == LightColor.Green))
+            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[0].Signals[0].CurrentLight) 
+                && (configuration.Intersections[0].SignalGroups[0].Signals[0].CurrentLight == LightColor.Green))
                 return true;
             //checking for concurrent green lights on "sunriserd_north_south" and "universityblvd_east_west" 
-            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[1].Signals[0].CurrentLight) && (configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == LightColor.Green))
+            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[1].Signals[0].CurrentLight) 
+                && (configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == LightColor.Green))
                 return true;
             //checking for concurrent yellow lights on "sunriserd_north_south" and "universityblvd_east_west" 
-            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[1].Signals[0].CurrentLight) && (configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == LightColor.Yellow))
+            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[1].Signals[0].CurrentLight) 
+                && (configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == LightColor.Yellow))
                 return true;
+
             return false;
-            
-            //Run for 2 minutes
-
-            simulator.Run(new TimeSpan(0, 2, 0));
-
-            //checking for concurrent green lights on "sunriserd_north_south" and "universityblvd_east_west_turnlanes" 
-            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[0].Signals[0].CurrentLight) && (configuration.Intersections[0].SignalGroups[0].Signals[0].CurrentLight == LightColor.Green))
-                return true;
-            //checking for concurrent green lights on "sunriserd_north_south" and "universityblvd_east_west" 
-            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[1].Signals[0].CurrentLight) && (configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == LightColor.Green))
-                return true;
-            //checking for concurrent yellow lights on "sunriserd_north_south" and "universityblvd_east_west" 
-            if ((configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == configuration.Intersections[0].SignalGroups[1].Signals[0].CurrentLight) && (configuration.Intersections[0].SignalGroups[2].Signals[0].CurrentLight == LightColor.Yellow))
-                return true;
-            return false;
-
-            
+                 
         }
     }
  }
