782c48550748a87a25c3bca54cca56080cb0e7f6
travismturner <travismack70@yahoo.com>
2017-08-09 20:28:17 -0500

    removed some commented out code I left from a previous merge conflict

--- TrafficControlSystem/IntersectionController.cs
+++ TrafficControlSystem/IntersectionController.cs
@@ -7,40 +7,6 @@ using System.Windows.Forms;
 
 namespace TrafficControlSystem
 { 
-//<<<<<<< HEAD
-//{
-//    /// <summary>
-//    /// A delegate definition used to pass the current intersection to the GUI
-//    /// </summary>
-//    /// <param name="intersection">This is an Intersection object that is the current state of the intersection in the system.</param>
-//    public delegate void UIEvent(Intersection intersection);
-
-//    /// <summary>
-//    /// An object of this time is passed to the GUI to allow it to subscribe to changes in the intersection
-//    /// and make those changes when an update event happens in the intersection.
-//    /// </summary>
-//    public class UISyncObject
-//    {
-//        /// <summary>
-//        /// A delegate UIEvent object used to pass the current intersection to the GUI.
-//        /// </summary>
-//        public event UIEvent TimeToUpdate;
-
-//        /// <summary>
-//        /// a method that triggers the GUI that the intersection has changed and it is time
-//        /// for the GUI to update.
-//        /// </summary>
-//        /// <param name="intersection">The Intersection object that is used by the system. </param>
-//        public void OnTimeToUpdate(Intersection intersection)
-//        {
-//            if (TimeToUpdate != null)
-//                TimeToUpdate(intersection);
-//        }
-//    }
-	
-//=======
-//{	
-//>>>>>>> 2af865207507281c1361388d4493d5801c26faa2
     /// <summary>
     /// IntersectionController Class
     /// </summary>
